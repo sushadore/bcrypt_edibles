d1ab2e755cb6e4d23cebce1626eca16182f25efb
Susha Dore <susha.dore@gmail.com>
2017-06-30 15:15:53 -0700

    pass review content_body minimum and maximum character unit test

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, presence: true
-  validates :content_body, presence: true
-  # , length: {minimum: 50, maximum: 250, too_short: "%{count} characters minimum", too_long: "%{count} characters maximum"}
+  validates :content_body, presence: true, length: { minimum: 50, maximum: 250 }
   validates :rating, presence: true, numericality: true, inclusion: { in: 1..5 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,6 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_numericality_of :rating }
   it { should validate_inclusion_of(:rating).in_range(1..5) }
+  it { should validate_length_of(:content_body).is_at_least(50)}
+  it { should validate_length_of(:content_body).is_at_most(250)}
 end
