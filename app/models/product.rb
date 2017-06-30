d3d0708fd10abb8f32bb26e914d2ffcda06466f5
Susha Dore <susha.dore@gmail.com>
2017-06-30 14:36:23 -0700

    pass product validation spec

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :reviews
-  # validates :name, presence: true
-  # validates :cost, presence: true
-  # validates :country_of_origin, presence: true
+  validates :name, presence: true
+  validates :cost, presence: true
+  validates :country_of_origin, presence: true
 end
