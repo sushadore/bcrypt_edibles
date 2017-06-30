fb8b7a6ea8a5d934e746852ba7bf7f8882a7d449
Susha Dore <susha.dore@gmail.com>
2017-06-30 15:08:07 -0700

    pass review rating numericality and range unit test

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :product
-  # validates :author, presence: true,
-  # validates :content_body, presence: true, length: {minimum: 50, maximum: 250, too_short: "%{count} characters minimum", too_long: "%{count} characters maximum"}
-  # validates :rating, presence: true, inclusion: {in: [1,5]}
+  validates :author, presence: true
+  validates :content_body, presence: true
+  # , length: {minimum: 50, maximum: 250, too_short: "%{count} characters minimum", too_long: "%{count} characters maximum"}
+  validates :rating, presence: true, numericality: true, inclusion: { in: 1..5 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to :product }
+  it { should validate_presence_of :author }
+  it { should validate_numericality_of :rating }
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
 end
