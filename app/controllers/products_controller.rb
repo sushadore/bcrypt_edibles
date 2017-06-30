b0071da3cac1a10de85bff0ff96cd8e8564122f2
Susha Dore <susha.dore@gmail.com>
2017-06-30 16:47:46 -0700

    add product creation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,23 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product has been added."
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :country_of_origin, :cost)
+  end
 end
