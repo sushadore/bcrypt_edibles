cc8d915bdc45162190aec3499c6fe1b942a32b3c
Susha Dore <susha.dore@gmail.com>
2017-06-30 18:26:43 -0700

    add review creation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,22 @@
 class ReviewsController < ApplicationController
 
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@review.product)
+    else
+      render :new
+    end
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
+  end
 end
