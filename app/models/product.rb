b196db93af6f87734dc6a5b157f5c0fa292cc8e0
Susha Dore <susha.dore@gmail.com>
2017-06-30 21:11:55 -0700

    add scopes for most reviews and most recent additions

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,13 @@ class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true
+
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :most_reviews, -> {(
+    select("products.*, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+    )}
 end
